fe45696cf677c64525e942bf1e66b512e4e381de
Daisuke Konishi <skydai1151@gmail.com>
2021-03-07 01:45:57 +0900

    タイトルリンクの構造修正
    PostListの簡単なスタイル追加

--- components/posts/PostList.tsx
+++ components/posts/PostList.tsx
@@ -3,21 +3,23 @@ import * as React from 'react'
 import { postType } from '../../types/api/post'
 import { routes } from '../../routes'
 
+import styles from './PostList.module.css'
+
 type postsProps = {
   posts: postType[]
 }
 
 const PostList: React.FC<postsProps> = (posts: postsProps) => (
-  <ul>
-    {posts.posts.map((post: postType) => {
-      return (
-        <li key={post.id}>
-          <Link href={routes.postDetail(post.id)}>
-            <span>{post.title}</span>
-          </Link>
-        </li>
-      )
-    })}
+  <ul className={styles.postList}>
+    {posts.posts.map((post: postType) => (
+      <li className={styles.postListItem} key={post.id}>
+        <Link href={routes.postDetail(post.id)}>
+          <a>
+            <span className={styles.postTitle}>{post.title}</span>
+          </a>
+        </Link>
+      </li>
+    ))}
   </ul>
 )
 
