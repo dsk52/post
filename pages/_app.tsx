60cb2b33bf90a3990ed644b74a5195e06b74c367
Daisuke Konishi <skydai1151@gmail.com>
2021-03-06 17:17:42 +0900

    globalなスタイルを全体で読み込むように調整

---
+++ pages/_app.tsx
@@ -0,0 +1,10 @@
+import type { AppProps } from 'next/app'
+
+import 'sanitize.css'
+import '../global.css'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
